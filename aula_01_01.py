8db78f1df2d1940fd69cf9256f1bdf9c05f49b3f
Luiz Alberto Moraes <80058540+LuizAlberto1984@users.noreply.github.com>
2024-11-13 18:38:15 -0300

    Add files via upload

---
+++ aula_01_01.py
@@ -0,0 +1,6 @@
+n1 = 10
+n2 = 5.6
+nome = "Luiz"
+idade = 50
+print('Sr(a) ', nome, ', a sua idade é ', idade)
+
---
+++ aula_01_02.py
@@ -0,0 +1,3 @@
+num = int(input('Informe um valor: '))
+result = num * 2
+print('O resultado do dobro do número é: ', result)
